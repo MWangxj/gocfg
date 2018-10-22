eac19361960d7182b0c44a9e2a1c146d864406fe
guishan <guishan@dian.so>
2018-10-22 14:19:15 +0800

    update the config

--- config.go
+++ config.go
@@ -7,16 +7,18 @@ import (
 )
 
 func Load(filePath string, gcfg interface{}) error {
-	var conf *ini.File
+	var (
+		conf *ini.File
+		err  error
+	)
 	if _, err := os.Stat(filePath); err != nil && os.IsExist(err) {
 		return errors.New("file not found")
 	}
-	conf, err := ini.Load(filePath)
-	if err != nil {
+	if conf, err = ini.Load(filePath); err != nil {
 		return errors.New("Loading config ini error : " + err.Error())
 	}
-	err = conf.MapTo(gcfg)
-	if err != nil {
+
+	if err = conf.MapTo(gcfg); err != nil {
 		return errors.New("map to struct error : " + err.Error())
 	}
 	return nil
